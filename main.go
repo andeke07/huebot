ff97e74846d5dc723bebacb533f7d2e112c16e9f
KAO12 <kevin.anderson2@sky.uk>
2021-12-21 22:10:48 -0500

    first pass at adding Bridges

--- main.go
+++ main.go
@@ -1,8 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	bridge "github.com/andeke07/huebot/components"
+)
 
 func main() {
-	fmt.Println("Hello, world")
-	bridge.callFromBridge()
+	// Set up a reader to read from stdio
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Welcome to Huebot!")
+	fmt.Println("Currently no Bridges configured")
+	fmt.Print("Would you like to configure a bridge now? (y/N): ")
+
+	// Get user's input
+	input, _ := reader.ReadString('\n')
+
+	if (strings.TrimSuffix(strings.ToLower(input), "\n")) == "y" {
+		fmt.Println("Searching for Bridges...")
+		bridge.AddNewBridge()
+	} else {
+		fmt.Println("Thanks for using Huebot!")
+	}
+
 }
